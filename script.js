209443cf88199e522d79687d28eb8086d1ec7f66
homemdeferro9990-debug <homemdeferro9990@gmail.com>
2026-04-28 10:45:14 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,33 +1,18 @@
-// CONFIG FIREBASE (troca pelos seus dados)
 const firebaseConfig = {
-  apiKey: "SUA_API_KEY",
+  apiKey: "SUA_KEY",
   authDomain: "SEU_PROJETO.firebaseapp.com",
   databaseURL: "https://SEU_PROJETO-default-rtdb.firebaseio.com",
-  projectId: "SEU_PROJETO",
 };
 
-// Inicializa
 firebase.initializeApp(firebaseConfig);
+
 const db = firebase.database();
 
-// Lê dados
 db.ref("sala").on("value", (snapshot) => {
   const data = snapshot.val();
 
-  if (!data) {
-    document.getElementById("status").innerText = "Sem dados";
-    return;
-  }
+  if (!data) return;
 
   document.getElementById("status").innerText =
     data.ocupado ? "🔴 Sala Ocupada" : "🟢 Sala Livre";
-
-  document.getElementById("status").className =
-    data.ocupado ? "status ocupado" : "status livre";
-
-  document.getElementById("atividade").innerText =
-    data.ultima;
-
-  document.getElementById("tempo").innerText =
-    data.tempo + " min";
 });
